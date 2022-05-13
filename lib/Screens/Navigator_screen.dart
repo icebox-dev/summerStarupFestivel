770413f57ef4305f4e07ebba721c519952e2ce7d
realAK47 <amalkrishnajk@gmail.com>
2022-05-13 14:20:20 +0530

    v1.0.1

---
+++ lib/Screens/Navigator_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:mmproto/Screens/map_screen.dart';
+ class NavigatorScreen extends StatelessWidget {
+   const NavigatorScreen({Key? key}) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       body: SafeArea(
+         child: Column(
+           children: [
+             SizedBox(height: 40,),
+             TextButton(onPressed: (){
+             }, child: Text("Login")),
+             SizedBox(height: 40,),
+             TextButton(onPressed: (){
+               Navigator.push(context, MaterialPageRoute(builder: (context)=> MapScreen()));
+             }, child: Text("Map")),
+             SizedBox(height: 40,),
+             TextButton(onPressed: (){
+             }, child: Text("title")),
+             SizedBox(height: 40,),
+             TextButton(onPressed: (){
+             }, child: Text("title")),
+             SizedBox(height: 40,),
+             TextButton(onPressed: (){
+             }, child: Text("title")),
+
+
+           ],
+         ),
+       ),
+     );
+   }
+ }
