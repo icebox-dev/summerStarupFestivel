c0f6237287afb24081c6ab8a972c0ecbd9e4b466
Ajay Mathew <ajayedathazhe@gmail.com>
2022-05-13 16:58:05 +0530

    v1.0.1

--- lib/Screens/Navigator_screen.dart
+++ lib/Screens/Navigator_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:mmproto/Screens/map_screen.dart';
+import 'package:mmproto/Screens/login_screen.dart';
  class NavigatorScreen extends StatelessWidget {
    const NavigatorScreen({Key? key}) : super(key: key);
 
@@ -12,6 +13,7 @@ import 'package:mmproto/Screens/map_screen.dart';
            children: [
              SizedBox(height: 40,),
              TextButton(onPressed: (){
+               Navigator.push(context, MaterialPageRoute(builder: (context) => loginScreen()));
              }, child: Text("Login")),
              SizedBox(height: 40,),
              TextButton(onPressed: (){
