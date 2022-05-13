3758b6090df357f83ef225fb577bc43ecb41505c
realAK47 <amalkrishnajk@gmail.com>
2022-05-13 22:55:55 +0530

    v0.1.0

--- lib/Screens/map_screen.dart
+++ lib/Screens/map_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 
 
 class MapScreen extends StatefulWidget {
@@ -13,12 +16,40 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+
+
+  List<Marker> _marker = [
+
+    // buildMarker(context,LatLng(lat, long+1),"2","mgchj"),
+    // buildMarker(context,LatLng(lat-1, long),"3","mgchj"),
+    // buildMarker(context,LatLng(lat, long-1),"4","mgchj"),
+    // buildMarker(context,LatLng(lat, long),"5","mgchj"),
+
+  ] ;
+  double lat = 10.525252080548244;
+  double long = 76.21449784404028;
+
+  List arr = [{"latLong":LatLng(10.525252080548244+1, 76.21449784404028),"id":"1","pid":"sdfgjuy"},
+    {"latLong":LatLng(10.525252080548244-1, 76.21449784404028),"id":"2","pid":"sdfgjuy"},
+    {"latLong":LatLng(10.525252080548244, 76.21449784404028),"id":"3","pid":"sdfgjuy"},
+    {"latLong":LatLng(10.525252080548244, 76.21449784404028-1),"id":"4","pid":"sdfgjuy"},
+    {"latLong":LatLng(10.525252080548244, 76.21449784404028+1),"id":"5","pid":"sdfgjuy"}];
+
+  void kkk(){
+    for(int i = 0;i<=4;i++){
+      _marker.add(buildMarker(context, arr[i]["latLong"], arr[i]["id"], arr[i]["pid"]));
+      print("**************************************************");
+      print(_marker);
+    }
+  }
+
+
   @override
-  
-  // initState(){
-  //   LocatingMe();
-  //   super.initState();
-  // }
+  initState(){
+    kkk();
+    super.initState();
+  }
+
   // void LocatingMe()async{
   //
   //   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
@@ -30,20 +61,16 @@ class _MapScreenState extends State<MapScreen> {
   // late Position currentLocation;
   //late LatLng latLongPosition;
 
-  double lat = 10.525252080548244;
-  double long = 76.21449784404028;
-  
- 
+
+  int count = 0;
+
+
+
   Widget build(BuildContext context) {
 
-    List<Marker> _marker = <Marker>[
-      Marker(markerId: MarkerId("1"),
-        position: LatLng(lat, long),
-        infoWindow: InfoWindow(
-          title: "you are here"
-        )
-      )
-    ] ;
+
+
+
 
 
     Completer<GoogleMapController> _controller = Completer();
@@ -67,6 +94,27 @@ class _MapScreenState extends State<MapScreen> {
     return Scaffold(
       body: SafeArea(
         child: GoogleMap( mapType: MapType.normal,
+          // onTap: (LatLng val){
+          //   print(val);
+          //   Marker newMarker = Marker(markerId:MarkerId("$count") ,position:LatLng(val.latitude,val.longitude),infoWindow: InfoWindow(title: "new"));
+          //   //
+          //   _newMarker.add(newMarker);
+          //   // _marker.addAll(_newMarker);
+          //
+          //
+          //
+          //   // print(count);
+          //   count++;
+          //   // print(_marker.length);
+          //   // print(_newMarker.length);
+          //   // print(_marker);
+          //
+          //   setState(() {});
+          // },
+          onLongPress: (val){
+            print("____________________________________________________________________________________${val}________________________________________________");
+          },
+
           initialCameraPosition: _kGooglePlex,
           myLocationEnabled: true,
           myLocationButtonEnabled: true,
@@ -74,9 +122,9 @@ class _MapScreenState extends State<MapScreen> {
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
-          markers:Set<Marker>.of(_marker),
+          markers:_marker.map((e) => e).toSet(),
         ),
-        ),
+      ),
       floatingActionButton: Transform.scale(
         scale: 1.2,
         child: FloatingActionButton.extended(
@@ -90,7 +138,99 @@ class _MapScreenState extends State<MapScreen> {
       ),
     );
   }
+
+  Marker buildMarker(BuildContext context,LatLng latLng,String id,String pid) {
+
+    return Marker(markerId: MarkerId(id),
+        position: latLng,
+        infoWindow: const InfoWindow(
+            title: "you are here"
+        ),
+        onTap: (){
+          showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (BuildContext context) => Bsheet(),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15),
+                    topLeft: Radius.circular(15)),
+              ));
+        }
+    );
+  }
 }
 
 
 
+class Bsheet extends StatelessWidget {
+  const Bsheet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData queryData = MediaQuery.of(context);
+
+    return Container(
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+              bottomRight: Radius.circular(0),
+              bottomLeft: Radius.circular(0))),
+      margin: EdgeInsets.only(left: 30, top: 40, bottom: 0, right: 30),
+      height: queryData.size.height * 0.30,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "Just the mail,",
+            style: TextStyle(
+              wordSpacing: 2,
+              fontSize: 25,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          Text(
+            "we got you covered !",
+            style: TextStyle(
+              fontSize: 25,
+              wordSpacing: 2,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          SizedBox(
+            width: queryData.size.width * 0.6,
+            height: 50,
+            child: TextField(
+              onChanged: (val) {},
+              decoration: InputDecoration(
+                  hintText: "tcr*******@gectcr.ac.in",
+                  hintStyle: TextStyle(letterSpacing: 3),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Text(
+              "Verify →",
+              textAlign: TextAlign.right,
+              style: TextStyle(fontSize: 20, color: Colors.blueAccent),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
